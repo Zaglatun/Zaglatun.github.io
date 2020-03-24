5542800760c22f252f5238687c9b37f127724840
Zaglatun <62351582+Zaglatun@users.noreply.github.com>
2020-03-24 13:43:27 +0300

    Revert "init commit"
    This reverts commit 22dc479c6cae5d5e6e1667ee4b3d5529c53d034c.

--- ajax.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-require 'class.php';
-$connect= new DB('mysql:host=localhost;dbname='.$_GET['database'],'root','');
-$connect->delete_table();
-//if($_GET['database']!=NULL && $_GET['table']== ''){$connect->show_table();}
-$connect->show_table();
-?>
